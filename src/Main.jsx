9eb242073345d79dba5028fe0aafbb9f67568bcd
fathasyaarliansyah@gmail.com <fathasyaarliansyah@gmail.com>
2022-12-24 16:21:19 +0700

    done

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { LandingPage, DetailProducts, Add, Edit, Delete } from "./pages"
-// import ProductItems from "./components/Constants/ProductItems"
 
 const router = createBrowserRouter([
 	{
@@ -11,18 +10,6 @@ const router = createBrowserRouter([
 	{
 		path: "/:id",
 		element: <DetailProducts />,
-	},
-	{
-		path: "/Add",
-		element: <Add/>,
-	},
-	{
-		path: "/Edit/:id",
-		element: <Edit/>,
-	},
-	{
-		path: "/Delete",
-		element: <Delete/>,
 	},
 ])
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,2 @@
 export { default as LandingPage } from "./LandingPage"
 export { default as DetailProducts } from "./DetailProducts"
-export { default as Add } from "./Add"
-export { default as Edit } from "./Edit"
-export { default as Delete } from "./Delete"
-
